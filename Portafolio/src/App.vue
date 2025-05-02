70c64c816000ae0f5982cb9e1aef9e1cdabe11bd
Daniela <jessot@hotmail.com>
2025-05-02 16:19:53 -0500

    s

--- Portafolio/src/App.vue
+++ Portafolio/src/App.vue
@@ -1,14 +1,13 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import AppBar from './components/AppBar.vue'
+
 import imageCentral from './components/imageCentral.vue'
 
 </script>
 
 <template>
     <imageCentral></imageCentral>
-  <AppBar></AppBar>
-  <div class="trans"></div>
+
 
 
   <RouterView />
